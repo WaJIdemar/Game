b56955b4cd368f23c48bf026f72516e47f27331d
WaJIdemar <zavlov_rf@mail.ru>
2021-04-16 23:46:30 +0500

    MiniRefactoring

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -19,6 +19,8 @@ namespace WindowsFormsApp1
         public Image gladiatorSheetRight;
         public Image gladiatorSheetLeft;
         public Entity player;
+        public Size oldClientSize;
+
         public Form1()
         {
             InitializeComponent();
@@ -26,47 +28,9 @@ namespace WindowsFormsApp1
             timer1.Interval = 1;
             timer1.Tick += new EventHandler(Update);
 
-            KeyUp += new KeyEventHandler(OnKeyUp);
-            KeyDown += new KeyEventHandler(OnPress);
+            KeyUp += new KeyEventHandler(MoveController.OnKeyUp);
+            KeyDown += new KeyEventHandler(MoveController.OnPress);
             Init();
-        }
-
-        public void OnKeyUp(object sender, KeyEventArgs e)
-        {
-            player.pressButtonMove = false;
-        }
-
-        public void OnPress(object sender, KeyEventArgs e)
-        {
-            if (!player.pressButtonMove && !player.isMoving)
-            {
-                switch (e.KeyCode)
-                {
-                    case Keys.W:
-                        player.dirY = -3;
-                        player.isMoving = true;
-                        player.SetAnimationConfiguration(1);
-                        break;
-                    case Keys.S:
-                        player.dirY = 3;
-                        player.isMoving = true;
-                        player.SetAnimationConfiguration(1);
-                        break;
-                    case Keys.A:
-                        player.dirX = -3;
-                        player.lastDirX = -3;
-                        player.isMoving = true;
-                        player.SetAnimationConfiguration(1);
-                        break;
-                    case Keys.D:
-                        player.dirX = 3;
-                        player.lastDirX = 3;
-                        player.isMoving = true;
-                        player.SetAnimationConfiguration(1);
-                        break;
-                }
-                player.pressButtonMove = true;
-            }
         }
 
         public void Init()
@@ -83,6 +47,9 @@ namespace WindowsFormsApp1
             player = new Entity((this.Width - MapController.cellSize * 2) / 2 + 7, (this.Height - MapController.cellSize * 2) / 2 - 7,
                 Hero.idleFrames, Hero.runFrames, Hero.attackFrames,
                 Hero.deathFrames, gladiatorSheetLeft, gladiatorSheetRight);
+
+            MoveController.AddPlayer(player);
+
             timer1.Start();
         }
 
@@ -98,27 +65,68 @@ namespace WindowsFormsApp1
                 timer1.Interval = 500;
                 player.SetAnimationConfiguration(0);
             }
+           
             Invalidate();
         }
 
         private void OnPaint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
+           
             MapController.DrawMap(g);
             player.PlayAnimation(g);
         }
     }
 }
-//public void StepOn(int dirX, int dirY, int repeats)
+
+//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.IO;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows.Forms;
+
+//namespace BardDungeon
 //{
-//    player.dirX = dirX;
-//    player.dirY = dirY;
-//    timer1.Interval = 50;
-//    player.isMoving = true;
-//    player.SetAnimationConfiguration(1);
-//    for (int i = 0; i < repeats; i++)
+//    public partial class Form1 : Form
 //    {
-//        Invalidate();
+//        //protected override void OnPaint(PaintEventArgs e)
+//        //{
+//        //          var graphics = e.Graphics;
+//        //              var image = Image.FromFile(@"C:\Users\Lenovo\source\repos\BardDungeon\BardDungeon\мозг.png");
+//        //          graphics.ScaleTransform(2f, 2f);
+//        //          graphics.DrawImage(image, new Point(200,0));
+//        //}
+
+//        public Form1()
+//        {
+//            var fSizeX = 600f;
+//            var fSizeY = 600f;
+//            ClientSize = new Size(600, 600);
+//            var centerX = ClientSize.Width / 2;
+//            var centerY = ClientSize.Height / 2;
+//            var timer = new Timer();
+//            var t = 0;
+//            timer.Interval = 40;
+//            timer.Tick += (sender, args) =>
+//            {
+//                var graphics = CreateGraphics();
+//                fSizeY = ClientSize.Height;
+//                fSizeX = ClientSize.Width;
+//                graphics.ScaleTransform(fSizeX / 600, fSizeY / 600);
+//                var fileName = new StringBuilder(@"Sprites\Snowman\Snowman");
+//                fileName.Append((t % 8).ToString() + ".png");
+//                var i = new Bitmap(Path.Combine(new DirectoryInfo(Directory.GetCurrentDirectory())
+//                .Parent.Parent.FullName.ToString(), fileName.ToString()));
+//                graphics.FillRectangle(Brushes.White, new Rectangle(0, 0, ClientSize.Width, ClientSize.Height));
+//                graphics.DrawImage(i, new PointF(centerX, centerY));
+//                t++;
+//            };
+//            timer.Start();
+//        }
 //    }
-//    timer1.Interval = 100;
 //}
