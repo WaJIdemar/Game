464e7c19e9f7dac58b69ec6e609fe1c3919b7b43
WaJIdemar <zavlov_rf@mail.ru>
2021-04-27 18:45:46 +0500

    Добавил класс костей

---
+++ WindowsFormsApp1/Entites/Die.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Движение.Entites
+{
+    public class Die
+    {
+        private int num_sides;
+        private Random rnd;
+        public Die(int num_sides = 6)
+        {
+            this.num_sides = num_sides;
+            rnd = new Random();
+        }
+
+        public int Roll()
+        {
+            return rnd.Next(num_sides);
+        }
+    }
+}
